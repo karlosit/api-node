aa9576467d9d549603a80ebab32b59a0a23aaa63
Carlos Olmedo <carlos.olmedodev@gmail.com>
2019-12-07 16:19:43 +0100

    [+] Acces Remote

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.json({succes: "Hello World!"});
 });
 
 app.get('/users', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     res.status(200).json({
         user: "carlosit"
     })
